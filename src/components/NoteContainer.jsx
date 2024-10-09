e4396aa27b7b7e7a3fb0ad857c3e1e97b487fdee
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-10-09 20:03:49 +0530

    add: link of assets

--- src/components/NoteContainer.jsx
+++ src/components/NoteContainer.jsx
@@ -10,12 +10,12 @@ function NoteContainer() {
   return (
     <div className={styles.mainContainer}>
     <div className={styles.noteContainer}>
-      <img src="\src\assets\note-bg.png" alt="note-bg" />
+      <img src="https://github.com/user-attachments/assets/b8781ba1-0d17-4725-ba2f-3f7c4cc1c1fe" alt="note-bg" />
       <h1>Pocket Notes</h1>
       <p className={styles.paragraph}>Send and receive messages without keeping your phone online.<br/>
       Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
     </div>
-    <p className={styles.endToEnd}><img src="\src\assets\lock.png" alt="lock.png" /> end-to-end encrypted</p>
+    <p className={styles.endToEnd}><img src="https://github.com/user-attachments/assets/9b047463-1c8d-4d29-9622-348386cb241a" alt="lock.png" /> end-to-end encrypted</p>
     </div>
   )
 }
